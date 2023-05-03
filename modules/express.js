0a1a738668353d431fd07566a3f2ad74179e4e98
DavidSilva32 <david.silvaxd32@gmail.com>
2023-05-02 23:36:03 -0300

    Update express.js

--- modules/express.js
+++ modules/express.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Conten Type: ${req.headers["content-type"]}`);
+  console.log(`Date: ${new Date()}`);
+
+  next();
+});
+
 app.get("/users", async (req, res) => {
   try {
     const users = await UserModel.find({});
@@ -33,7 +41,31 @@ app.post("/users", async (req, res) => {
 
     res.status(201).json(user);
   } catch (error) {
-    return res.status(500).send(error.message);
+    res.status(500).send(error.message);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
